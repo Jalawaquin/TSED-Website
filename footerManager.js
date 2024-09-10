2e62ab97ecf9a2bb0b199cc3b8864375800cc9f1
Jalawaquin <57761181+Jalawaquin@users.noreply.github.com>
2024-09-10 09:13:42 -0500

    footer mananager
    added footer manager so i don't have to type footer a bunch. also makes it easier to edit

---
+++ footerManager.js
@@ -0,0 +1,11 @@
+class CustomFooter extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <footer>
+          © 2024 Powered by Joaquin's blood sweat and tears
+        </footer>
+        `
+    }
+}
+
+customElements.define('custom-footer', CustomFooter)
